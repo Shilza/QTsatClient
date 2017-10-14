e3313cb32aa73ae7d42a58f70da336319a5c8dea
Toleckk <31159587+Toleckk@users.noreply.github.com>
2017-10-14 22:33:08 +0300

    Sessions

--- udpserver.cpp
+++ udpserver.cpp
@@ -36,8 +36,8 @@ UDPServer::UDPServer(QObject *parent) :
     list.push_back("Shilza");
     list.push_back("0");
     QSqlQuery query;
-    query.prepare("SELECT ID FROM users WHERE Nickname=? AND Password=?");
-    query.bindValue(list.at(0), list.at(1));
+    query.prepare("SELECT ID FROM users WHERE Nickname=?");
+    query.bindValue(0, list.at(0));
     query.exec();
     QString id="";
     while ( query.next() )
