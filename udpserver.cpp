2cefbca780a774375b8282796e6fc86f2567f047
Toleckk <31159587+Toleckk@users.noreply.github.com>
2017-10-16 20:05:03 +0300

    Session checker

--- udpserver.cpp
+++ udpserver.cpp
@@ -38,10 +38,11 @@ UDPServer::UDPServer(QObject *parent) :
         while(true){
             answers.clear();
             unsigned int time=QDateTime::currentDateTime().toTime_t();
-            qDebug()  << QString(0).toUtf8();
+            QByteArray a;
+            a.append("0");
             for(int i=0; i<sessions.size(); i++)
                 if(time > sessions[i].get()->time+10)
-                    systemSocket->writeDatagram(QString(i).toUtf8(), sessions[i].get()->IP, 49002);
+                    systemSocket->writeDatagram(a, sessions[i].get()->IP, 49002);
             std::this_thread::sleep_for(std::chrono::seconds(2));
 
             for(int i=0; i<sessions.size(); i++)
